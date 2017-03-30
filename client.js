88d0ae2aaee417e16ddc20f31b2b2d67d3696f9c
Colin Wageman <colinwageman@Colins-MacBook-Pro.local>
2017-03-30 11:38:57 -0700

    Working system... need to update for pi locally.

--- client.js
+++ client.js
@@ -1,34 +1,58 @@
 var os = require('os');
+var nrc = require('node-run-cmd');
 var socket = require('socket.io-client')('http://localhost:8080/television');
 
-
-var ifaces = os.networkInterfaces();
-
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
-
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
-
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      console.log(ifname + ':' + alias, iface.address);
-    } else {
-      // this interface has only one ipv4 adress
-      console.log(ifname, iface.address);
-    }
-    ++alias;
-  });
-});
-
-
 socket.on('connect', function(){
-	console.log('Generic Connection');
+  console.log('Connected to System');
+
+  var ifaces = os.networkInterfaces();
+
+
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log(ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        console.log(ifname, iface.address);
+        // if (ifname == 'wlan0') {
+        if (ifname == 'en1') {
+          
+          var info = {
+            hostname: os.hostname(),
+            ip: iface.address
+          };
+
+          socket.emit('computerrunning', info);
+        }
+      }
+      ++alias;
+    });
+  });
+
 });
 
-socket.on('connected', function(data) {
-	console.log('Connected: ' + data);
+socket.on('shutdown', function(data) {
+  console.log('shutting down');
+  nrc.run('sudo shutdown -h now');
+  // process.exit();
+});
+
+socket.on('startVideo', function(data) {
+  console.log('Starting Video');
+  // nrc.run('sudo supervisorctl start video_looper');
+});
+
+socket.on('stopVideo', function(data) {
+  console.log('Stopping Video');
+  // nrc.run('sudo supervisorctl stop video_looper');
 });
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var express = require('express');
 var app  = express();
+var nrc = require('node-run-cmd');
 
-
-
+var alltvs = [];
+var shutdownFlag = false;
 
 app.use( express.static(__dirname + '/public') );
 
@@ -16,20 +17,68 @@ var server = http.createServer(app);
 var io = require('socket.io').listen( server );
 server.listen(8080);
 
+var tvnsp = io.of('/television');
+
 io.on('connection', function (socket) {
 	console.log("New Connection");
 
-	socket.emit('news', { hello: 'world' });
-
-	socket.on('my other event', function (data) {
-		console.log(data);
+	socket.on('turnoff', function() {
+		console.log('Turning Off TVs');
+		shutdownFlag = true;
+		tvnsp.emit('shutdown');
 	});
-});
 
-var tvnsp = io.of('/television');
+	socket.on('startVideos', function() {
+		console.log('Starting Playback');
+		tvnsp.emit('startVideo');
+		nrc.run('sudo supervisorctl start video_looper');
+	});
+
+	socket.on('stopVideos', function() {
+		console.log('Stopping Playback');
+		tvnsp.emit('stopVideo');
+		nrc.run('sudo supervisorctl stop video_looper');
+	});
+
+	notify();
+});
 
 tvnsp.on('connection', function(socket) {
 	console.log('television connected');
 
 	socket.emit('connected', 'Welcome');
+
+	socket.on('computerrunning', function (data) {
+		socket.info = data;
+		alltvs.push(socket);
+
+		notify();		
+	});
+
+	socket.on('disconnect', function(data) {
+		console.log('disconnection');
+
+		var i = alltvs.indexOf(socket);
+		alltvs.splice(i, 1);
+
+		notify();
+
+		if (shutdownFlag) {
+			console.log('ShuttingDown');
+			nrc.run('sudo shutdown -h now');
+			// process.exit();
+		}
+	});
+
 });
+
+function notify() {
+
+	var list = [];
+	alltvs.forEach(function(s) {
+		list.push(s.info);
+	});
+	
+
+	io.emit('notification', { connected: list });
+}
